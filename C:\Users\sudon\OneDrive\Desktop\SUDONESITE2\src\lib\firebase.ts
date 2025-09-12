535c8c0baf100ddd2822806c5eb7f909c5ef8c6f
Lucho Mingrone <lucionmingrone@gmail.com>
2025-09-12 03:09:49 +0000

    C:\Users\sudon\OneDrive\Desktop\SUDONESITE2>firebase login --reauth "fir

--- C:\Users\sudon\OneDrive\Desktop\SUDONESITE2\src\lib\firebase.ts
+++ C:\Users\sudon\OneDrive\Desktop\SUDONESITE2\src\lib\firebase.ts
@@ -3,16 +3,15 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration will be populated here automatically
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "sudone-v2-d8839.firebaseapp.com",
-  projectId: "sudone-v2-d8839",
-  storageBucket: "sudone-v2-d8839.appspot.com",
-  messagingSenderId: "1039893963328",
-  appId: "1:1039893963328:web:77f7228a8d7a6e12aa1176",
-  measurementId: "G-XXXXXXXXXX"
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID",
+  measurementId: "G-MEASUREMENT_ID"
 };
 
 // Initialize Firebase
