add3b19cb99297c671d22f5459f1b6d42541ced3
Lucho Mingrone <lucionmingrone@gmail.com>
2025-09-12 03:17:03 +0000

    Estoy desde el navegador mozzila escribiendote, la cuenta era la correct

--- C:\Users\sudon\OneDrive\Desktop\SUDONESITE2\src\lib\firebase.ts
+++ C:\Users\sudon\OneDrive\Desktop\SUDONESITE2\src\lib\firebase.ts
@@ -1,18 +1,10 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApp, getApps } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration will be populated here automatically
-const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
-  measurementId: "G-MEASUREMENT_ID"
-};
+// Your web app's Firebase configuration will be populated here automatically by Firebase App Hosting.
+const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG || '{}');
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
