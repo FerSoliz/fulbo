3ac86e3415f0d6ac242fbb8f100e6a920a11344a
Lucho Mingrone <lucionmingrone@gmail.com>
2025-09-12 01:24:28 +0000

    Pero tampoco apretaste tu en publicar la que creaste

--- C:\Users\sudon\OneDrive\Desktop\SUDONESITE2\src\lib\firebase.ts
+++ C:\Users\sudon\OneDrive\Desktop\SUDONESITE2\src\lib\firebase.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -8,14 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
-  authDomain: "sudone-v2.firebaseapp.com",
-  projectId: "sudone-v2",
-  storageBucket: "sudone-v2.appspot.com",
-  messagingSenderId: "338959982829",
-  appId: "1:338959982829:web:96e001a1e479a61325d759",
+  authDomain: "sudone-v2-d8839.firebaseapp.com",
+  projectId: "sudone-v2-d8839",
+  storageBucket: "sudone-v2-d8839.appspot.com",
+  messagingSenderId: "1039893963328",
+  appId: "1:1039893963328:web:77f7228a8d7a6e12aa1176",
   measurementId: "G-XXXXXXXXXX"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
