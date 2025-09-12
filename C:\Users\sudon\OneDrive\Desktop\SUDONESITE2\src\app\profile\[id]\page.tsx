4fe705c0ea10a5be7f21ccb8d647e3f14f3deb8c
Lucho Mingrone <lucionmingrone@gmail.com>
2025-09-12 22:48:05 +0000

    Avisame cuando vas ahacer las cosas como te digo

--- C:\Users\sudon\OneDrive\Desktop\SUDONESITE2\src\app\profile\[id]\page.tsx
+++ C:\Users\sudon\OneDrive\Desktop\SUDONESITE2\src\app\profile\[id]\page.tsx
@@ -245,7 +245,7 @@ export default function ProfilePage() {
                                     <TooltipProvider>
                                         <Tooltip>
                                             <TooltipTrigger>
-                                                <Image src="https://i.postimg.cc/8cm263zS/verificado.png" alt="Verificado" width={24} height={24} />
+                                                <Image src="/images/verificado.png" alt="Verificado" width={24} height={24} />
                                             </TooltipTrigger>
                                             <TooltipContent>
                                                 <p>Usuario Verificado</p>
