d0109fdb4438644570d9b8469571736b501930a7
Lucho Mingrone <lucionmingrone@gmail.com>
2025-09-12 10:22:33 +0000

    Busca otra ruta para hacer esto puede ser esta src\lib\data u otra que n

--- C:\Users\sudon\OneDrive\Desktop\SUDONESITE2\src\lib\data.ts
+++ C:\Users\sudon\OneDrive\Desktop\SUDONESITE2\src\lib\data.ts
@@ -1,4 +1,5 @@
 
+
 export interface User {
   id: string;
   name: string;
@@ -118,18 +119,19 @@ export const leagues = [
 ];
 
 export const defaultVisitor: User = {
-    id: 'visitor-0',
-    name: 'VISITANTE',
-    avatar: 'https://avatar.vercel.sh/visitor.png',
-    location: 'Navegando',
-    isVerified: false,
-    role: 'user',
+    id: '1',
+    name: 'Lucio Mingrone',
+    email: 'lucionmingrone@gmail.com',
+    avatar: 'https://i.postimg.cc/xTT3zpg1/MARADONA-Y-EL-PURO-e1630357319461.jpg',
+    location: 'Buenos Aires, Argentina',
+    isVerified: true,
+    role: 'admin',
     sudpoints: 0,
     baseSudpoints: 0,
     league: 'Bronce',
     division: 4,
     stats: { partidosJugados: 0, victorias: 0, empates: 0, derrotas: 0, goles: 0, asistencias: 0, amarillas: 0, rojas: 0, mvps: 0 },
-};
+  };
 
 
 export const users: User[] = [
@@ -325,3 +327,4 @@ export const initialNotifications: Notification[] = [
         createdAt: new Date(Date.now() - 1000 * 60 * 60 * 48).toISOString(), // hace 2 días
     },
 ];
+
