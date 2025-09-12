83ae55f229777a9dd56a1fbf33a662dfd1788990
Lucho Mingrone <lucionmingrone@gmail.com>
2025-09-12 09:08:41 +0000

    Perfecto pero por que el visitante no ve el sponor de monster? usa la ru

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -197,6 +197,17 @@ export function PageHeader() {
                 />
             </form>
         </div>
+         <div className="px-4 pb-2">
+            <Link href="https://www.monsterenergy.com" target="_blank" rel="noopener noreferrer">
+                <Image
+                    src="https://i.postimg.cc/PNGVGZ92/banner-monster.png"
+                    alt="Monster Energy Banner"
+                    width={1200}
+                    height={150}
+                    className="w-full h-auto rounded-lg"
+                />
+            </Link>
+        </div>
     </header>
   );
 }
