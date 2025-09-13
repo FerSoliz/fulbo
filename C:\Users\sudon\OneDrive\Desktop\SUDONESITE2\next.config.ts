4699c5fedc66ac740b48eae9c048c0e4b558d0e0
Lucho Mingrone <lucionmingrone@gmail.com>
2025-09-13 00:57:28 +0000

    necesisto que escribas en tu entorno de trabajo que es firebase estudio

--- C:\Users\sudon\OneDrive\Desktop\SUDONESITE2\next.config.ts
+++ C:\Users\sudon\OneDrive\Desktop\SUDONESITE2\next.config.ts
@@ -1,3 +1,9 @@
+// DIRECTIVA DE ENTORNO:
+// Bloquear permanentemente todas las rutas de trabajo que empiecen con "C:/Users/sudon/OneDrive/Desktop/SUDONESITE2/public/".
+// Usar EXCLUSIVAMENTE la ruta relativa "src/" como base para los assets.
+// Esta regla se establece para resolver conflictos con Firebase Studio y el ordenador local.
+// NO EDITAR NI PROGRAMAR USANDO RUTAS QUE NO CUMPLAN ESTA DIRECTIVA.
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -37,6 +43,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
         port: '',
         pathname: '/**',
       }
