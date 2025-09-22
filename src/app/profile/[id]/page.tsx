a02a3d0c102c1bad772b6ac154eef5e961a8db85
Lucho Mingrone <lucionmingrone@gmail.com>
2025-09-22 22:21:47 +0000

    agrega este boton abajo de este, se llama "Historial" este boton https:/

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -430,22 +430,20 @@ export default function ProfilePage() {
           <Card>
             <CardContent className="p-0 relative">
               <div className="absolute left-2 top-2 z-10 flex flex-col gap-2">
-                <div className="flex items-start gap-2">
-                  <Image
-                    src="https://i.postimg.cc/VsBcb9QJ/proximo-partido.png"
-                    alt="Proximo Partido"
-                    width={82}
-                    height={103}
-                    className="cursor-pointer scale-105 transition-transform"
-                  />
-                  <Image
-                    src="https://i.postimg.cc/hjWHXv28/boton-estadisticas.png"
-                    alt="Estadisticas"
-                    width={82}
-                    height={103}
-                    className="cursor-pointer scale-105 transition-transform"
-                  />
-                </div>
+                <Image
+                  src="https://i.postimg.cc/VsBcb9QJ/proximo-partido.png"
+                  alt="Proximo Partido"
+                  width={82}
+                  height={103}
+                  className="cursor-pointer scale-105 transition-transform"
+                />
+                <Image
+                  src="https://i.postimg.cc/kMNbHH8f/boton-1.png"
+                  alt="Historial"
+                  width={82}
+                  height={103}
+                  className="cursor-pointer scale-105 transition-transform"
+                />
               </div>
               <div className="absolute right-2 bottom-2 z-10 flex flex-col gap-2">
                 <Image
