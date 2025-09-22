df1654b2a307810e147a7845bc85304e6d2d73d8
Lucho Mingrone <lucionmingrone@gmail.com>
2025-09-22 22:03:56 +0000

    bien, pero recuerda qeu tienen que tener un espacio entre ellos, que no

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -430,7 +430,7 @@ export default function ProfilePage() {
           <Card>
             <CardContent className="p-0 relative">
               <div className="absolute left-2 top-2 z-10 flex flex-col gap-2">
-                 <div className="flex items-start gap-0">
+                 <div className="flex items-start gap-2">
                     <Image
                       src="https://i.postimg.cc/kMNbHH8f/boton-1.png"
                       alt="Historial"
