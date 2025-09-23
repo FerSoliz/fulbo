3a969f4282a779b45d41c26f9188850f2430df57
Lucho Mingrone <lucionmingrone@gmail.com>
2025-09-23 01:29:25 +0000

    debajo de jugador agrega esta insigniabadge https://i.postimg.cc/c1KwdNC

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -654,6 +654,9 @@ export default function ProfilePage() {
                   ? "Administrador"
                   : "Jugador"}
               </CardDescription>
+              <div className="flex items-center gap-4 pt-2">
+                <DivisionBadge league={league} division={division} />
+              </div>
             </CardHeader>
             <CardContent>
               {currentUser &&
