ababa3b591d8d79442da97897e55690067a9dfbb
Lucho Mingrone <lucionmingrone@gmail.com>
2025-09-23 08:05:45 +0000

    la imagne tiene que estar en la mejor resolucion y calidad que se pueda

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -370,7 +370,7 @@ export default function ProfilePage() {
               src={profileBackground}
               alt="Imagen de fondo del perfil"
               layout="fill"
-              className="object-contain rounded-t-lg"
+              className="object-cover rounded-t-lg"
               priority
             />
           )}
