8a1769c9dcb5b3a875cdfaef4f148a753e3a1ecb
fernandomsoliz <fernandomsoliz@gmail.com>
2025-10-25 03:42:34 +0000

    2510 2

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -163,7 +163,7 @@ export default function ProfilePage() {
 
       {view === 'buttons' ? (
         <motion.div 
-            className="grid grid-cols-2 md:grid-cols-3 gap-3 pt-4"
+            className="grid grid-cols-2 md:grid-cols-3 gap-[6px] pt-1"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -20 }}
